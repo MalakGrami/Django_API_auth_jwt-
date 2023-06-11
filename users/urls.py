c7c69e052cd092c599af521726e6c014787bf0fc
Malak1Grami <gramimalak@gmail.com>
2023-06-11 15:37:09 +0100

    add  refreshToken

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import RegiterView,loginView,UserView,LogoutView
+from .views import RegiterView,loginView,UserAPIView,LogoutAPIView,RefreshAPIView
 
 urlpatterns = [
     path('register',RegiterView.as_view()),
     path('login',loginView.as_view()),
-    path('user',UserView.as_view()),
-    path('logout',LogoutView.as_view()),
+    path('user',UserAPIView.as_view()),
+    path('refreshToken',RefreshAPIView.as_view()),
+    path('logout',LogoutAPIView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from users.models import User
+from rest_framework.authentication import get_authorization_header
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import APIException, AuthenticationFailed
+
+from .authentication import create_access_token, create_refresh_token, decode_access_token, decode_refresh_token
 from .serializers import UserSerializers
-import jwt,datetime
+from .models import User
+
 
 # Create your views here.
 
@@ -16,51 +23,59 @@ class RegiterView(APIView):
         return Response(serializer.data)
 
 class loginView(APIView):
+
     def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
+        user = User.objects.filter(email=request.data['email']).first()
 
-        user = User.objects.filter(email=email).first()
-        if user is None:
-            raise AuthenticationFailed('user not found')
-        if not user.check_password(password):
-            raise AuthenticationFailed('incorrect password')
-        payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
-        }
-        token = jwt.encode(payload, 'secret', algorithm='HS256')
-        try:
-            token = token.decode('utf-8')  # Appel à decode si disponible
-        except AttributeError:
-            pass
+        if not user:
+            raise APIException('Invalid credentials!')
+
+        if not user.check_password(request.data['password']):
+            raise APIException('Invalid credentials!')
+
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
         response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
-        response.data = {
-            'jwt': token
-        }
-        return response
-    
-class UserView(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('jwt')
-        if not token:
-            raise AuthenticationFailed('unauthenticated!')
-        try:
-            payload = jwt.decode(token, 'secret', algorithms=['HS256'])  # Specify the algorithm
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('unauthenticated!')
 
-        user = User.objects.get(id=payload['id'])  # Use "id" instead of "payload['id']"
-        serializer = UserSerializers(user)
-        return Response(serializer.data)
-    
-class LogoutView(APIView):
-    def post(self,request):
-        response=Response()
-        response.delete_cookie('jwt')
+        response.set_cookie(key='refreshToken', value=refresh_token, httponly=True)
         response.data = {
-            'message':'success'
+            'token': access_token
+        }
+
+        return response
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+
+            user = User.objects.filter(pk=id).first()
+
+            return Response(UserSerializers(user).data)
+
+        raise AuthenticationFailed('unauthenticated')
+
+
+class RefreshAPIView(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refreshToken')
+        id = decode_refresh_token(refresh_token)
+        access_token = create_access_token(id)
+        return Response({
+            'token': access_token
+        })
+
+
+class LogoutAPIView(APIView):
+    def post(self, _):
+        response = Response()
+        response.delete_cookie(key="refreshToken")
+        response.data = {
+            'message': 'success'
         }
         return response
